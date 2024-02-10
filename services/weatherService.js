05553f5e1dd8bd5378b5ed78bd57eebf3b2606ee
sandeepkumarks <kssandeep18@gmail.com>
2024-02-10 03:54:06 -0800

    [Fix] Removed access key authentication for DynamoDB

--- services/weatherService.js
+++ services/weatherService.js
@@ -3,10 +3,10 @@ import 'dotenv/config';
 
 const client = new DynamoDBClient({ 
     region: 'us-east-1', 
-    credentials:{
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    } 
+    // credentials:{
+    //     accessKeyId: process.env.AWS_ACCESS_KEY,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    // } 
 });
 
 let apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
